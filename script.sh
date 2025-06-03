e4afcaab7fd5923926631a1f72aa0203a7ab449e
TheSem29 <semsemsem1541@gmail.com>
2025-06-03 22:32:12 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,53 +1,61 @@
 #!/bin/bash
 
-# Configuration
-DISK="/dev/sda"  # Для VirtualBox используем /dev/sda
-HOSTNAME="arch-vbox"
+# Конфигурация (измените под свои нужды)
+DISK="/dev/nvme0n1"
+HOSTNAME="arch-pc"
 USERNAME="user"
 TIMEZONE="Europe/Moscow"
 EFI_SIZE="512M"
 ROOT_SIZE="100%"
 KERNEL="linux"
 
-# Root check
+# Проверка на root
 if [ "$(id -u)" -ne 0 ]; then
-    echo "This script must be run as root!" >&2
+    echo "Этот скрипт должен запускаться с правами root!" >&2
     exit 1
 fi
 
-# Part 1: Disk partitioning
-echo -e "\n\033[1;32m[1] Partitioning disk ($DISK)\033[0m"
+# Проверка наличия NVMe диска
+if [ ! -e "$DISK" ]; then
+    echo -e "\033[1;31m[ОШИБКА] Диск $DISK не найден!\033[0m"
+    echo "Доступные диски:"
+    lsblk -d -o NAME,SIZE,MODEL | grep -E 'nvme|sd[a-z]'
+    exit 1
+fi
+
+# Часть 1: Разметка диска
+echo -e "\n\033[1;32m[1] Разметка диска ($DISK)\033[0m"
 parted -s "$DISK" mklabel gpt
 parted -s "$DISK" mkpart primary fat32 1MiB "$EFI_SIZE"
 parted -s "$DISK" set 1 esp on
 parted -s "$DISK" mkpart primary ext4 "$EFI_SIZE" "$ROOT_SIZE"
 
-# Formatting
-echo -e "\n\033[1;32m[2] Formatting partitions\033[0m"
-mkfs.vfat -F32 "${DISK}1"
-mkfs.ext4 -F "${DISK}2"
+# Форматирование
+echo -e "\n\033[1;32m[2] Форматирование разделов\033[0m"
+mkfs.vfat -F32 "${DISK}p1"
+mkfs.ext4 -F "${DISK}p2"
 
-# Mounting
-mount "${DISK}2" /mnt
+# Монтирование
+mount "${DISK}p2" /mnt
 mkdir -p /mnt/boot/efi
-mount "${DISK}1" /mnt/boot/efi
+mount "${DISK}p1" /mnt/boot/efi
 
-# Part 2: Base system installation
-echo -e "\n\033[1;32m[3] Installing base packages\033[0m"
+# Часть 2: Установка базовой системы
+echo -e "\n\033[1;32m[3] Установка базовых пакетов\033[0m"
 pacman -Sy --noconfirm archlinux-keyring
 pacstrap /mnt base base-devel "$KERNEL" "$KERNEL-headers" linux-firmware \
-          nano bash-completion grub efibootmgr networkmanager \
-          git reflector
+          nano vim bash-completion grub efibootmgr networkmanager \
+          git reflector sudo
 
-# Mirror optimization
+# Оптимизация зеркал
 arch-chroot /mnt reflector --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
-# Generate fstab
+# Генерация fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Part 3: System configuration in chroot
-arch-chroot /mnt /bin/bash <<EOF
-# Timezone and locale
+# Часть 3: Настройка системы в chroot
+arch-chroot /mnt /bin/bash <<'EOF'
+# Настройка времени и локали
 ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 hwclock --systohc
 sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
@@ -57,120 +65,68 @@ echo "LANG=ru_RU.UTF-8" > /etc/locale.conf
 echo "KEYMAP=ru" > /etc/vconsole.conf
 echo "FONT=cyr-sun16" >> /etc/vconsole.conf
 
-# Network configuration
+# Настройка сети
 echo "$HOSTNAME" > /etc/hostname
 echo -e "127.0.0.1 localhost\n::1 localhost\n127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
 systemctl enable NetworkManager
 
-# Users and passwords
+# Пользователь и пароли
 echo "root:root" | chpasswd
 useradd -m -G wheel,audio,video,storage -s /bin/bash "$USERNAME"
 echo "$USERNAME:$USERNAME" | chpasswd
 sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
-# Bootloader
+# Загрузчик
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# VirtualBox Guest Additions (вместо NVIDIA)
-pacman -S --noconfirm virtualbox-guest-utils mesa
-systemctl enable vboxservice
+# Драйверы NVIDIA (если нужно)
+if lspci | grep -i nvidia; then
+    pacman -S --noconfirm nvidia nvidia-utils nvidia-settings opencl-nvidia
+    sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+    echo "options nvidia-drm modeset=1" > /etc/modprobe.d/nvidia.conf
+    echo -e "WLR_NO_HARDWARE_CURSORS=1\nWLR_RENDERER=vulkan\nLIBVA_DRIVER_NAME=nvidia\nGBM_BACKEND=nvidia-drm\n__GLX_VENDOR_LIBRARY_NAME=nvidia" >> /etc/environment
+    mkinitcpio -P
+fi
 
-# Hyprland environment
-sudo -u $USERNAME git clone https://aur.archlinux.org/yay-bin.git /home/$USERNAME/yay-bin
-cd /home/$USERNAME/yay-bin && sudo -u $USERNAME makepkg -si --noconfirm
+# Установка yay от имени пользователя
+sudo -u $USERNAME bash <<'USEREOF'
+git clone https://aur.archlinux.org/yay-bin.git ~/yay-bin
+cd ~/yay-bin && makepkg -si --noconfirm
+USEREOF
+
+# Установка Hyprland и компонентов
 sudo -u $USERNAME yay -S --noconfirm hyprland xdg-desktop-portal-hyprland waybar \
     rofi kitty thunar file-roller qt5-wayland qt6-wayland \
     sddm network-manager-applet pipewire wireplumber pipewire-audio \
-    pipewire-alsa pipewire-pulse noto-fonts-cjk noto-fonts-emoji ttf-hack \
-    swaybg wl-clipboard xf86-input-libinput brightnessctl
+    pipewire-alsa pipewire-pulse noto-fonts-cjk noto-fonts-emoji \
+    ttf-hack ttf-fira-code swaybg wl-clipboard xf86-input-libinput \
+    brightnessctl
 
-# SDDM and config setup
+# Настройка SDDM
 systemctl enable sddm
-sudo -u $USERNAME mkdir -p /home/$USERNAME/.config/hypr
-cp /usr/share/hyprland/examples/hyprland.conf /home/$USERNAME/.config/hypr/
-chown -R $USERNAME:$USERNAME /home/$USERNAME/.config
 
-# Basic Hyprland configuration for VirtualBox
-cat > /home/$USERNAME/.config/hypr/hyprland.conf <<'HYPRCONF'
-exec-once = waybar
-exec-once = swaybg -i ~/wallpaper.jpg
-exec-once = dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-exec-once = systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+# Конфигурация Hyprland
+if [ -f "/usr/share/hyprland/examples/hyprland.conf" ]; then
+    sudo -u $USERNAME mkdir -p /home/$USERNAME/.config/hypr
+    cp /usr/share/hyprland/examples/hyprland.conf /home/$USERNAME/.config/hypr/
+    chown -R $USERNAME:$USERNAME /home/$USERNAME/.config
+else
+    echo "ERROR: Hyprland не установлен корректно!" >&2
+    exit 1
+fi
 
-# Используем программный рендеринг для VirtualBox
-env = WLR_RENDERER,pixman
+# Загрузка обоев
+sudo -u $USERNAME curl -o /home/$USERNAME/wallpaper.jpg \
+    https://raw.githubusercontent.com/linuxdotexe/nordic-wallpapers/master/wallpapers/nordic-wallpaper.png
 
-monitor=,highres,auto,1
-
-input {
-    kb_layout = us,ru
-    kb_options = grp:alt_shift_toggle
-    follow_mouse = 1
-    touchpad {
-        natural_scroll = yes
-    }
-}
-
-general {
-    sensitivity = 1.0
-    main_mod = SUPER
-}
-
-decoration {
-    rounding = 5
-    blur = no # Отключаем размытие для производительности
-}
-
-animations {
-    enabled = no # Отключаем анимации для VirtualBox
-}
-
-dwindle {
-    pseudotile = yes
-    preserve_split = yes
-}
-
-master {
-    new_is_master = true
-}
-
-bind = SUPER, RETURN, exec, kitty
-bind = SUPER, Q, killactive,
-bind = SUPER, E, exec, thunar
-bind = SUPER, V, togglefloating,
-bind = SUPER, R, exec, rofi -show drun
-
-bind = SUPER, left, movefocus, l
-bind = SUPER, right, movefocus, r
-bind = SUPER, up, movefocus, u
-bind = SUPER, down, movefocus, d
-
-bind = SUPER, 1, workspace, 1
-bind = SUPER, 2, workspace, 2
-bind = SUPER, 3, workspace, 3
-bind = SUPER, 4, workspace, 4
-
-bind = SUPER SHIFT, 1, movetoworkspace, 1
-bind = SUPER SHIFT, 2, movetoworkspace, 2
-bind = SUPER SHIFT, 3, movetoworkspace, 3
-bind = SUPER SHIFT, 4, movetoworkspace, 4
-
-bind = , XF86AudioRaiseVolume, exec, pactl set-sink-volume @DEFAULT_SINK@ +5%
-bind = , XF86AudioLowerVolume, exec, pactl set-sink-volume @DEFAULT_SINK@ -5%
-bind = , XF86AudioMute, exec, pactl set-sink-mute @DEFAULT_SINK@ toggle
-HYPRCONF
-
-# Download wallpaper
-sudo -u $USERNAME curl -o /home/$USERNAME/wallpaper.jpg https://raw.githubusercontent.com/linuxdotexe/nordic-wallpapers/master/wallpapers/nordic-wallpaper.png
-
-# Fix permissions
+# Фикс прав
 chown -R $USERNAME:$USERNAME /home/$USERNAME
 EOF
 
-# Completion
-echo -e "\n\033[1;32m[!] Installation completed!\033[0m"
-echo -e "Run these commands to reboot:"
+# Завершение
+echo -e "\n\033[1;32m[!] Установка завершена!\033[0m"
+echo -e "Выполните команды для перезагрузки:"
 echo "umount -R /mnt"
 echo "reboot"
